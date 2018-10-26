6710d9ad499ee4b7c54fd61a20f614e5e5cdc3fa
Ashley <ashley ashley.noble@gmail.com>
2018-10-26 09:56:57 -0400

    Revert "Simple JavaFX Application demonstrating switching to the GUI thread and properly shutting down the application"
    This reverts commit f6acf3ec0fb8131198cb3ccf07f4d3de0287cbbd.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,17 +1,16 @@
 package sample;
 
-import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable
@@ -20,69 +19,39 @@ public class Controller implements Initializable
     @FXML
     TextField tfText;
 
-    private Thread dateThread;
-    private DateFormat df = new SimpleDateFormat("hh:mm:ss");
+    @FXML
+    ComboBox cbNames;
+    //uncomment to reference the collection created in the FXML
+//    @FXML
+//    ObservableList newItems;
 
-    boolean isRunning = true;
+    //the observable list
+    ObservableList<String> names = FXCollections.observableArrayList("Mary", "Bob", "sam");
+
     private final StringProperty twoWayInput = new SimpleStringProperty("");
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        String temp = tfText.getText();
+        //do a quick sanity check before adding the text to the list
+        if (temp.equals("") || names.contains(temp))
+        {
+            return;
+        }
+
+        names.add(temp);
+        //Uncomment to bind to the collection defined in the FXML file
+        //  newItems.add(temp);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
         tfText.textProperty().bindBidirectional(twoWayInputProperty());
-
-        dateThread = new Thread(this::handleThread);
-        dateThread.start();
-
-    }
-
-    private void handleThread()
-    {
-        while (isRunning)
-        {
-            String dateStr = ("current time: " + df.format(new Date()));
-
-            Platform.runLater(() -> {
-                setTwoWayInput(dateStr);
-            });
-
-
-            try
-            {
-                Thread.sleep(2000);
-            } catch (InterruptedException iex)
-            {
-
-            }
-        }
-    }
-
-    @FXML
-    private void handleButtonAction(ActionEvent event)
-    {
-
-
-        if (isRunning)
-        {
-            isRunning = false;
-        }
-        else
-        {
-            isRunning = true;
-
-            dateThread = new Thread(this::handleThread);
-            dateThread.setName("running date time");
-            dateThread.start();
-        }
-
-
-    }
-
-
-    public void Dispose()
-    {
-     isRunning = false;
-
+        //this links the collection to the combobox
+         cbNames.setItems(names);
     }
 
     public String getTwoWayInput()
@@ -100,4 +69,13 @@ public class Controller implements Initializable
         this.twoWayInput.set(twoWayInput);
     }
 
+    public ObservableList<String> getNames()
+    {
+        return names;
+    }
+
+    public void setNames(ObservableList<String> names)
+    {
+        this.names = names;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,40 +1,23 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application
-{
-    Controller c;
+public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        FXMLLoader loader = new FXMLLoader();
-
-        Parent root = loader.load(getClass().getResourceAsStream("sample.fxml"));
-        c = loader.getController();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Bindings");
         primaryStage.setScene(new Scene(root, 150, 150));
         primaryStage.show();
-        primaryStage.setOnCloseRequest(e -> handleExit());
     }
 
 
-    private void handleExit()
-    {
-        c.Dispose();
-        Platform.exit();
-        System.exit(0);
-
-    }
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 }
